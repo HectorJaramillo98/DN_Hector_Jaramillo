0ce87ac1c24a596aa82fa99f9a838317f4e6f4f1
HectorJaramillo <hector.jaramillo@webpoint.mx>
2021-11-24 23:04:27 -0800

    Ejemplos y tutoriales finalizados

---
+++ 01_OOP/E2/DN_1.2_Hector_Jaramillo_v1.0/AudioManager/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Auto
+    {
+        public virtual void SoundVehicle()
+        {
+            Console.WriteLine("I'm a vehicle");
+        }
+    }
+}
---
+++ 01_OOP/E2/DN_1.2_Hector_Jaramillo_v1.0/DN_1.1_Hector_Jaramillo_v1.0/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace DN_1._1_Hector_Jaramillo_v1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DN_1.2_Hector_Jaramillo_v1.0"; // For better presentation
+            int remaining = 1;
+            while (remaining < 11)
+            {
+                Console.WriteLine("Intentos realizados: " + remaining);
+                Console.WriteLine("Escriba el vehiculo para reproducir su sonido!");
+
+                string type = Console.ReadLine().ToLower();
+
+                Auto auto = null;
+
+                switch (type)
+                {
+                    case "automovil":
+                        auto = new Automovil();
+                        break;
+
+                    case "camion":
+                        auto = new Camion();
+                        break;
+
+                    case "tren":
+                        auto = new Tren();
+                        break;
+
+                    default:
+                        Console.WriteLine("EL vehiculo no ha sido encontrado!");
+                        System.Threading.Thread.Sleep(1000);
+                        break;
+                }
+
+                if (auto != null)
+                {
+                    auto.SoundVehicle();
+                    remaining++; // The count only incremet if the sound is playing
+                    Console.Clear(); // For better presentation
+                }
+            }
+        }
+    }
+}
