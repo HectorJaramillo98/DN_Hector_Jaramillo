f1e7f0c607cbe1134db84a51f78343b99ecee2f0
HectorJaramillo <hector.jaramillo@webpoint.mx>
2021-12-14 11:07:55 -0800

    tutorial 6 completed

---
+++ 03_NetProjects/T6/WebApi.WindowsClient/Api.Entities/ZoneWeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Api.Entities
+{
+    public class ZoneWeatherForecast
+    {
+        public Zone Zone { get; set; }
+
+        public IEnumerable<WeatherForecast> WeatherForecast { get; set; }
+    }
+}
