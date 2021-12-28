5fb591ab000474b563f105c670d750d930f086d1
HectorJaramillo <hector.jaramillo@webpoint.mx>
2021-12-27 16:24:18 -0800

    SQL module

---
+++ 04_SQLStandard/T2/DN_4.2_Hector_Jaramillo_v1.0/company.sql
@@ -0,0 +1,6 @@
+CREATE TABLE company (
+  `idcompany` INT NOT NULL,
+  `companyname` VARCHAR(100),
+  `location` CHAR(2),
+  PRIMARY KEY (`idcompany`)
+  );
---
+++ 04_SQLStandard/T3/DN_4.3_Hector_Jaramillo_v1.0/inner_join_city.sql
@@ -0,0 +1,3 @@
+select ci.city_id, ci.city, ci.last_update, co.country from country co
+inner join city ci on co.country_id = ci.country_id
+where co.country_id = 2;
