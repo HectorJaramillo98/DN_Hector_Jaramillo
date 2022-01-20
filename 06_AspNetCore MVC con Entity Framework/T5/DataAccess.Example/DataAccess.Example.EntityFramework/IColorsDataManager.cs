0633ea0fe869dcec60295ddfdab898beaacdaa53
HectorJaramillo <hector.jaramillo@webpoint.mx>
2022-01-19 16:17:22 -0800

    completed 4,5

---
+++ 06_AspNetCore MVC con Entity Framework/T5/DataAccess.Example/DataAccess.Example.EntityFramework/IColorsDataManager.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Example.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Example.EntityFramework
+{
+    public interface IColorsDataManager
+    {
+        public void Insert(Color color);
+        public void Update(Color color);
+
+        Color Get(int id);
+
+        public IList<Color> GetAll();
+
+        public void Delete(int id);
+
+        
+    }
+}
---
+++ 06_AspNetCore MVC con Entity Framework/T5/DataAccess.Example/DataAccess.Example.EntityFramework/IVehiclesDataManager.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Example.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Example.EntityFramework
+{
+    public interface IVehiclesDataManager
+    {
+        public void Insert(Vehicle vehicle);
+        public void Update(Vehicle vehicle);
+
+        Vehicle Get(int id);
+
+        public IList<Vehicle> GetAll();
+
+        public void Delete(int id);
+    }
+}
