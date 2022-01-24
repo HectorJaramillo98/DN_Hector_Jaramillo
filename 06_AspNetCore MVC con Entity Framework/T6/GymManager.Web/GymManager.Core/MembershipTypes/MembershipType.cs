f39db9b116dbdaa13cba159809a7ea6affe0c553
HectorJaramillo <hector.jaramillo@webpoint.mx>
2022-01-23 20:30:06 -0800

    7 completed

---
+++ 06_AspNetCore MVC con Entity Framework/T6/GymManager.Web/GymManager.Core/MembershipTypes/MembershipType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Core.MembershipTypes
+{
+    public class MembershipType
+    {
+            public int Id { get; set; }
+
+            [StringLength(100)]
+            [Required]
+            public string Name { get; set; }
+
+            [DataType(DataType.Currency)]
+            [Required]
+            public double Cost { get; set; }
+
+            public DateTime CreatedOn { get; } = DateTime.Now;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Enter a valid Duration")]
+        public int Duration { get; set; }     
+    }
+}
---
+++ 06_AspNetCore MVC con Entity Framework/T6/GymManager.Web/GymManager.Web/Models/MemberViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GymManager.Web.Models
+{
+    public class MemberViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(15)]
+        [Required(ErrorMessage = "Debe ingresar el nombre")]
+        public string Name { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string LastName { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDay { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        public bool AllowNewsletter { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+    }
+}
