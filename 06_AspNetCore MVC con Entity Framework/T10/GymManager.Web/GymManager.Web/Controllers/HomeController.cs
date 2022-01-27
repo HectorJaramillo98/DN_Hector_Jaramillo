1fb3c095e7e05d7a74636ef384d05fa2f3218da3
HectorJaramillo <hector.jaramillo@webpoint.mx>
2022-01-26 21:41:30 -0800

    T10 completed

---
+++ 06_AspNetCore MVC con Entity Framework/T10/GymManager.Web/GymManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GymManager.AplicationsServices.Members;
+using GymManager.Core.Members;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GymManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMembersAppService _membersAppService;
+
+        public HomeController(IMembersAppService membersAppService)
+        {
+            _membersAppService = membersAppService;
+        }
+        public IActionResult Index()
+        {
+            var members = _membersAppService.GetMembersAsync();
+            return View(members);
+        }
+    }
+}
